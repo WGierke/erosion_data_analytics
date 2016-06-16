bdb218a818a2de88614054001cdd5ebe6d89a191
Willi Gierke <willi.gierke@student.hpi.uni-potsdam.de>
2016-06-16 15:52:35 +0200

    added transformation with one hot encoding

--- process.py
+++ process.py
@@ -1,15 +1,20 @@
 import os
 import pandas as pd
+from transformation import encode_onehot
 
-processed_file = '/data/processed.csv'
-
-def processed_data() -> pd.DataFrame:
-    """Create or read DataFrame"""
-    file_path_full = os.getcwd() + processed_file
+def read_data() -> pd.DataFrame:
+    """Read DataFrame"""
+    file_path_full = os.getcwd() + '/data/processed.csv'
 
     if os.path.isfile(file_path_full):
         return pd.DataFrame.from_csv(file_path_full, sep=';')
 
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+	"""Transform data"""
+	df = encode_onehot(df, cols=['Rock type', 'Climate zone main'])
+	return df
+
 if __name__ == '__main__':
-	data = processed_data()
+	data = read_data()
+	data = transform(data)
 	print(data)
---
+++ transformation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
+
+def encode_onehot(df: pd.DataFrame, cols):
+    vec = DictVectorizer()
+    vec_data = pd.DataFrame(vec.fit_transform(df[cols].to_dict(outtype='records')).toarray())
+    vec_data.columns = vec.get_feature_names()
+    vec_data.index = df.index
+
+    df = df.drop(cols, axis=1)
+    df = df.join(vec_data)
+    return df
